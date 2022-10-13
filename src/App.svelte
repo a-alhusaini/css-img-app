5d8dbdad78c1108bd870296771285ca00e384ea2
Abdullah Alhusaini <arvg3a@gmail.com>
2022-10-13 07:07:18 +0300

    added initial ACE editor setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,10 @@
 <script>
   import * as HtmlToImg from "html-to-image";
+  import ace from "ace-builds/src/ace.js";
+  import "ace-builds/src/ext-language_tools";
+  import "ace-builds/src/ext-error_marker";
+  import { onMount } from "svelte";
+
   let html = `
 <!-- root id is what gets rendered -->
 <div id="root">
@@ -14,13 +19,9 @@
 `;
   let dataUrl = "";
   let inPreviewTab = true;
-  let width = "";
-  let height = "";
 
   async function createImage() {
     let options = {};
-    if (width != "") options.width = width;
-    if (height != "") options.height = height;
 
     inPreviewTab = true;
 
@@ -30,22 +31,42 @@
       inPreviewTab = false;
     }, 100);
   }
+
+  onMount(() => {
+    ace.config.set("basePath", "/ace/");
+    ace.config.set("workerPath", "/ace/");
+
+    ace.require("ace/ext/language_tools");
+    ace.require("ace/ext/error_marker");
+    let config = {
+      enableBasicAutocompletion: true,
+      enableLiveAutocompletion: true,
+    };
+
+    let htmlEditor = ace.edit("html");
+    htmlEditor.setTheme("ace/theme/chrome");
+    htmlEditor.session.setMode("ace/mode/html");
+    htmlEditor.session.on("change", () => {
+      html = htmlEditor.getValue();
+    });
+    htmlEditor.setOptions(config);
+
+    let cssEditor = ace.edit("css");
+    cssEditor.setTheme("ace/theme/chrome");
+    cssEditor.session.setMode("ace/mode/css");
+    cssEditor.session.on("change", () => {
+      css = cssEditor.getValue();
+    });
+    cssEditor.setOptions(config);
+  });
 </script>
 
 <main>
   <div class="code-input">
-    <textarea class="code-area" spellcheck="false" bind:value={html} />
-    <textarea class="code-area" spellcheck="false" bind:value={css} />
-  </div>
-  <div class="inputs">
-    <div>
-      <span>Width</span>
-      <input bind:value={width} type="text" name="width" />
+    <div on:input={(val) => console.log(val)} id="html" class="code-area">
+      {html}
     </div>
-    <div>
-      <span>Height</span>
-      <input bind:value={height} type="text" name="height" />
-    </div>
+    <div id="css" class="code-area">{css}</div>
   </div>
   <div class="panes">
     <div on:click={() => (inPreviewTab = true)} class="pane">Preview</div>
@@ -103,24 +124,5 @@
     font-size: 1.5rem;
     padding: 1rem;
     text-align: center;
-  }
-
-  .inputs {
-    display: flex;
-    margin: 0 auto;
-    width: fit-content;
-    margin-bottom: 1rem;
-    margin-top: 1rem;
-    gap: 1rem;
-  }
-
-  .inputs > div > span {
-    font-size: 1.5rem;
-  }
-  .inputs > div > input {
-    display: block;
-    margin-top: 0.5rem;
-    padding: 0.5rem;
-    border-radius: 8px;
   }
 </style>
